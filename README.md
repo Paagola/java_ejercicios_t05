b83ac418811ae432c0e51e4adc1f6e5fd57c5cf1
Paagola <victorpagola.w@gmail.com>
2025-10-22 18:36:27 +0200

    new: exercise 5, 6 and 7 done

--- README.md
+++ README.md
@@ -45,8 +45,90 @@ Muestra los multiplos de 5 de 0 a 100 utilizando un bucle `do-while`.
 
 ## EJERCICIO 4
 
-Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle for.
+Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle `for`.
 
 ##  `EJEMPLO`
 
 > 320 300 280 260 240 220 180 160
+
+<br>
+
+---
+
+<br>
+
+## EJERCICIO 5
+
+Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle `while`.
+
+##  `EJEMPLO`
+
+> 320 300 280 260 240 220 180 160
+
+<br>
+
+---
+
+<br>
+
+## EJERCICIO 6
+
+Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle `do-while`.
+
+##  `EJEMPLO`
+
+> 320 300 280 260 240 220 180 160
+
+<br>
+
+---
+
+<br>
+
+## EJERCICIO 7
+
+Realiza el control de acceso a una caja fuerte. La combinació será un número de 4 cifras. El programa nos pedirá la combinaciónpara abrirla. Si no acertamos, se nos mostrará el mensaje "Lo siento, esa no es la combinación" y si acertamos se nos dirá "La caja fuerte se ha abierto satisfactoriamente."
+
+##  `EJEMPLO 1`
+
+> Introduzca la clavade la caja fuerte: 1234
+>
+> Clave Incorrecta!
+>
+> Introduzca la clavade la caja fuerte: 3424
+>
+> Clave Incorrecta!
+> 
+> Introduzca la clavade la caja fuerte: 1234
+> 
+> Clave Incorrecta!
+> 
+> Introduzca la clavade la caja fuerte:  5235
+>
+> Clave Incorrecta!
+>
+> Lo siento, ha agotado las 4 oportunidades.
+
+##  `EJEMPLO 2`
+
+> Introduzca la clavade la caja fuerte: 1242
+>
+> Clave Incorrecta!
+>
+> Introduzca la clavade la caja fuerte: 4321
+>
+> Clave Incorrecta!
+> 
+> Introduzca la clavade la caja fuerte: 3412
+> 
+> Clave Incorrecta!
+> 
+> Introduzca la clavade la caja fuerte: 8888
+>
+> Ha abierto la caja fuerte
+
+##  `EJEMPLO 3`
+
+> Introduzca la clavade la caja fuerte: 8888
+>
+> Ha abierto la caja fuerte
--- src/App.java
+++ src/App.java
@@ -8,6 +8,9 @@ public class App {
                 EJERCICIO 2 -> 2   |
                 EJERCICIO 3 -> 3   |
                 EJERCICIO 4 -> 4   |
+                EJERCICIO 5 -> 5   |
+                EJERCICIO 6 -> 6   |
+                EJERCICIO 7 -> 7   |
                 """);
         System.out.print("-> ");
         int num = Integer.parseInt(System.console().readLine());
@@ -23,9 +26,9 @@ public class App {
                                 debajo del otro, en una sola columna; o bien uno al lado del otro, en una misma fila.
                                         """);
 
-                int numero_uno = 5;
+                int numero = 5;
                 for (int i = 0; i < 21; i += 1) {
-                    System.out.print(numero_uno * i + " ");
+                    System.out.print(numero * i + " ");
                 }
                 System.out.println();
                 break;
@@ -37,15 +40,14 @@ public class App {
                                 Muestra dos números múltiplos de 5 de 0 a 100 utilizando un bucle while.
                                 """);
 
-                int numero_dos = 5;
+                numero = 5;
                 int i = 0;
                 while (i < 21) {
-                    System.err.print(numero_dos * i + " ");
+                    System.err.print(numero * i + " ");
                     i += 1;
                 }
                 System.out.println();
                 break;
-
 
             case 3: // EJERCICIO 3
                 System.out.println(ut.GREEN_BOLD + "EJERCICIO 3" + ut.RESET);
@@ -54,30 +56,82 @@ public class App {
                                 Muestra los multiplos de 5 de 0 a 100 utilizando un bucle do-while.
                                 """);
 
-                int numero_tres = 5;
+                numero = 5;
                 i = 0;
 
                 do {
-                    System.out.print(numero_tres * i + " ");
+                    System.out.print(numero * i + " ");
                     i += 1;
                 } while (i < 21);
                 System.err.println();
                 break;
-            
+
             case 4: // EJERCICIO 4
-                 System.out.println(ut.GREEN_BOLD + "EJERCICIO 4" + ut.RESET);
+                System.out.println(ut.GREEN_BOLD + "EJERCICIO 4" + ut.RESET);
                 System.out.println(
                         """
-                        Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle for.
-                                """);
-                
-                int numero_cuatro;
-                for (numero_cuatro = 340; numero_cuatro >= 160; numero_cuatro -= 20);
-                    System.out.println(numero_cuatro + " ");
+                                Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle for.
+                                        """);
 
-
+                for (numero = 340; numero >= 160; numero -= 20)
+                    ;
+                System.out.println(numero + " ");
                 break;
-                            
+
+            case 5: // EJERCICIO 5
+                System.out.println(ut.GREEN_BOLD + "EJERCICIO 5" + ut.RESET);
+                System.out.println(
+                        """
+                                Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle while.
+                                    """);
+
+                numero = 340;
+                while (numero > 160) {
+                    numero -= 20;
+                    System.out.print(numero + " ");
+                }
+                break;
+
+            case 6: // EJERCICIO 6
+                System.out.println(ut.GREEN_BOLD + "EJERCICIO 6" + ut.RESET);
+                System.out.println(
+                        """
+                                Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle do-while.
+                                    """);
+                numero = 340;
+                do {
+                    numero -= 20;
+                    System.out.print(numero + " ");
+
+                }while(numero >= 160);
+                
+            break;
+
+            case 7: // EJERCICIO 7
+                System.out.println(ut.GREEN_BOLD + "EJERCICIO 7" + ut.RESET);
+                System.out.println(
+                        """
+                            Realiza el control de acceso a una caja fuerte. La combinació será un número de 4 cifras. 
+                            El programa nos pedirá la combinaciónpara abrirla. 
+                            Si no acertamos, se nos mostrará el mensaje "Lo siento, esa no es la combinación" y si acertamos 
+                            se nos dirá "La caja fuerte se ha abierto satisfactoriamente."
+                                    """);
+                String intento;
+                i = 1;
+                do {
+                    intento = System.console().readLine("Introduzca la clave de la caja fuerte -> ");
+                    i++;
+                    if (!intento.equals("8888")) {
+                        System.out.println("Contraseña Incorrecta!");
+                    }
+
+                }while(!intento.equals("8888") && i <= 4);
+                if (intento.equals("8888")) {
+                    System.out.println("La contraseña es correcta!");
+                } else {
+                    System.out.println("Se ha excedido el número de intentos!!");
+                }
+            break;
 
             default:
                 System.err.println("El ejercicio que has seleccionado no existe.");
