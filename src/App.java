05c0befd20312c07b82ded35bbcfbc418c7b9a6a
Paagola <victorpagola.w@gmail.com>
2025-10-28 23:33:58 +0100

    "new: exercise 24, 32 and 33 done"

--- src/App.java
+++ src/App.java
@@ -3,15 +3,18 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         System.out.println("""
-                                                ELIGE UN EJERCICIO
-                ---------------------------------------------------------------------
-                EJERCICIO 1 -> 1   | EJERCICIO 8 -> 8   | EJERCICIO 15 -> 15 | EJERCICIO 22 -> 22 |
-                EJERCICIO 2 -> 2   | EJERCICIO 9 -> 9   | EJERCICIO 16 -> 16 | EJERCICIO 23 -> 23 |
-                EJERCICIO 3 -> 3   | EJERCICIO 10 -> 10 | EJERCICIO 17 -> 17 | EJERCICIO 24 ->    |
-                EJERCICIO 4 -> 4   | EJERCICIO 11 -> 11 | EJERCICIO 18 -> 18 | EJERCICIO 25 ->    |
-                EJERCICIO 5 -> 5   | EJERCICIO 12 -> 12 | EJERCICIO 19 -> 19 | EJERCICIO 26 -> 26 |
-                EJERCICIO 6 -> 6   | EJERCICIO 13 -> 13 | EJERCICIO 20 -> 20 |
-                EJERCICIO 7 -> 7   | EJERCICIO 14 -> 14 | EJERCICIO 21 -> 21 |
+                                                 ELIGE UN EJERCICIO
+                -----------------------------------------------------------------------------------
+                EJERCICIO 1 -> 1   | EJERCICIO 11 -> 11 | EJERCICIO 21 -> 21 | EJERCICIO 31 -> 31 |
+                EJERCICIO 2 -> 2   | EJERCICIO 12 -> 12 | EJERCICIO 22 -> 22 | EJERCICIO 32 -> 32 |
+                EJERCICIO 3 -> 3   | EJERCICIO 13 -> 13 | EJERCICIO 23 -> 23 | EJERCICIO 33 -> 33 |
+                EJERCICIO 4 -> 4   | EJERCICIO 14 -> 14 | EJERCICIO 24 -> 24 | EJERCICIO 34 -> 34 |
+                EJERCICIO 5 -> 5   | EJERCICIO 15 -> 15 | EJERCICIO 25 ->    |
+                EJERCICIO 6 -> 6   | EJERCICIO 16 -> 16 | EJERCICIO 26 -> 26 |
+                EJERCICIO 7 -> 7   | EJERCICIO 17 -> 17 | EJERCICIO 27 -> 27 |
+                EJERCICIO 8 -> 8   | EJERCICIO 18 -> 18 | EJERCICIO 28 -> 28 |
+                EJERCICIO 9 -> 9   | EJERCICIO 19 -> 19 | EJERCICIO 29 -> 29 |
+                EJERCICIO 10 -> 10 | EJERCICIO 20 -> 20 | EJERCICIO 30 ->    |
                 """);
         int num = Integer.parseInt(System.console().readLine("-> "));
         System.out.printf("%n%n");
@@ -526,6 +529,7 @@ public class App {
 
             default:
                 System.out.println("El ejercicio que has seleccionado no existe.");
+                break;
 
             case 22: // EJERCICIO 22
                 System.out.println(ut.GREEN_BOLD + "EJERCICIO 22" + ut.RESET);
@@ -561,9 +565,9 @@ public class App {
                                     """);
                 i = 0;
                 suma_num = 0;
-                    do {
+                do {
                     numero = Integer.parseInt(System.console().readLine("-> "));
-                    suma_num += numero; 
+                    suma_num += numero;
                     i++;
                 } while (suma_num < 10000);
                 System.out.println("Ha introducido un total de " + i + " números.");
@@ -575,10 +579,40 @@ public class App {
                 System.out.println(ut.GREEN_BOLD + "EJERCICIO 24" + ut.RESET);
                 System.out.println(
                         """
-                                PENDIENDE !!!
+                            Escribe un programa que lea un número n e imprima una pirámide de números con n filas
+                            como se muestra en los siguientes ejemplos:
                                     """);
-            
-            break;
+                altura = Integer.parseInt(System.console().readLine("Introduce la altura de la pirámide"));
+
+                y = 1;
+                aux = altura;
+                numeron = 0;
+                for (i = 0; i < altura; i++) {
+                    aux -= 1;
+                    numero = 0;
+                    for (int j = 0; j < aux; j++) {
+                        System.out.print(" ");
+                    }
+
+                    for (int j = 0; j < y; j++) {
+                        if (numero > (int)Math.ceil(y / 2)) {
+                            numeron -= 1;
+                            System.out.print(numeron);
+                        } else {
+                            numero += 1;
+                            System.out.print(numero);
+                            numeron = numero;
+                        } 
+                    } 
+
+                    for (int j = 0; j < aux-1; j++) {
+                        System.out.print(" ");
+                    }
+                    System.out.println(" ");
+                    y += 2;
+                }
+
+                break;
 
             case 25: // EJERCICIO 25
                 System.out.println(ut.GREEN_BOLD + "EJERCICIO 25" + ut.RESET);
@@ -587,16 +621,15 @@ public class App {
                                 PENDIENTE !!!
                                     """);
 
-            break;
+                break;
 
-
-             case 26: // EJERCICIO 26
+            case 26: // EJERCICIO 26
                 System.out.println(ut.GREEN_BOLD + "EJERCICIO 26" + ut.RESET);
                 System.out.println(
                         """
-                            Realiza un programa que pida primero un número y a continuación un dígito. El programa nos debe dat la posición (o posiciones), 
-                            contanto de izquierda a derecha en el número introducido
-                                    """);
+                                Realiza un programa que pida primero un número y a continuación un dígito. El programa nos debe dat la posición (o posiciones),
+                                contanto de izquierda a derecha en el número introducido
+                                        """);
                 String numeroString = System.console().readLine("Introduce un número: ");
                 char numerocaracter = System.console().readLine("Introduce un número: ").charAt(0);
 
@@ -605,17 +638,203 @@ public class App {
                         System.err.print((i + 1) + " ");
                     }
                 }
+                break;
 
-                 case 27: // EJERCICIO 27
+            case 27: // EJERCICIO 27
                 System.out.println(ut.GREEN_BOLD + "EJERCICIO 27" + ut.RESET);
                 System.out.println(
                         """
-                            
+                                Escribe un programa que muestre, cuente y sume los múltiplos de 3 que hay entre 1 y un número leido por
+                                teclado.
                                     """);
+                numero = Integer.parseInt(System.console().readLine("Introduce un número entero mayor que 1: "));
+                suma_num = 0;
+                int veces = (int) Math.floor(numero / 3);
+
+                for (i = 1; i <= veces; i++) {
+                    System.out.print(i * 3 + " ");
+                    suma_num += (i * 3);
                 }
+                System.out.printf("%nDesde 1 hasta %d hay %d múltiplos de 3 y suman %d.", numero, veces,
+                        (int) suma_num);
+                break;
+
+            case 28: // EJERCICIO 28
+                System.out.println(ut.GREEN_BOLD + "EJERCICIO 28" + ut.RESET);
+                System.out.println(
+                        """
+                                Escribe un programa que calcule el factorial de un número entero leido por teclado.
+                                    """);
+                numero = Integer.parseInt(System.console().readLine("Por favor, introduzca un número entero: "));
+                int fact = 1;
+
+                for (i = numero; i > 0; i--) {
+                    fact *= i;
+                }
+                System.out.println(numero + "! = " + fact);
+                break;
+
+            case 29: // EJERCICIO 29
+                System.out.println(ut.GREEN_BOLD + "EJERCICIO 29" + ut.RESET);
+                System.out.println(
+                        """
+                                Escribe un programa que muestre por pantalla todos los números enteros positivos menores a uno leído por
+                                teclado que no sean divisible entre otro tamnbién leido por teclado de igual forma.
+                                    """);
+
+                numero = Integer.parseInt(
+                        System.console().readLine("Introduzca un número entero positivo (relatvamente grande): "));
+                numeron = Integer
+                        .parseInt(System.console().readLine("Introduzca otro número (relativamente pequeño): "));
+                System.out.printf(
+                        "Los números entero positivos menores a %d que no son divisibles entre %d son los siguientes: %n",
+                        numero, numeron);
+
+                for (i = 1; i <= numero; i++) {
+                    if (i % numeron != 0) {
+                        System.out.print(i + " ");
+                    }
+                }
+                break;
+
+            case 30: // EJERCICIO 30
+                System.out.println(ut.GREEN_BOLD + "EJERCICIO 30" + ut.RESET);
+                System.out.println(
+                        """
+                                Realiza un programa que calcule las horas transcurridas entre dos horas de dos días de la semana.
+                                No se tendrán en cuenta los minutos ni los segundos. El día de la semana se puede pedir como un número
+                                (del 1 al 7) o como una cadena (de "lunes" a "domingo"). Se debe comprobar que el usuario introduce los datos
+                                correctamente y que el segundo día es posterior al primero.
+
+                                PENDIENTE!!!
+                                    """);
+                break;  
+
+            case 31: // EJERCICIO 31
+                System.out.println(ut.GREEN_BOLD + "EJERCICIO 31" + ut.RESET);
+                System.out.println(
+                        """
+                                Realiza un programa que pinte la letra L por pantalla hecha con asterisco. El programa pedirá la altura.
+                                El palo horizontal tendrá una longitud de la mitad (división entera entre 2) de la altura más uno.
+                                    """);
+                numero = Integer.parseInt(System.console().readLine("Introduzca la altura de la L: "));
+
+                for (i = 0; i < numero - 1; i++) {
+                    System.out.println("*");
+                }
+                for (i = 0; i < (int) Math.ceil(numero / 2) + 1; i++) {
+                    System.out.print("* ");
+                }
+                break;
+
+            case 32: // EJERCICIO 32
+                System.out.println(ut.GREEN_BOLD + "EJERCICIO 32" + ut.RESET);
+                System.out.println(
+                        """
+                                Escribe un programa que, dado un número entero positivo, diga cuáles son y cuánto suman los dígitos pares.
+                                Los dígitos pares se deben mostrar en orden, de izquierda a derecha. Usa long en lugar de int doende sea necesario,
+                                para admitir números largos.
+                                    """);
+
+                do {
+                    numero = Integer.parseInt(System.console().readLine("Introduce un número entero positivo: "));
+                } while (numero < 0);
+
+                String sumapar = "";
+                numeroString = numero + "";
+                int suma_num_int = 0;
                 
+                for (i=0; i < numeroString.length(); i++) {
+                    
+                    digitosn = Character.getNumericValue(numeroString.charAt(i));
+                    if (digitosn % 2 == 0) {
+                        sumapar = sumapar + (digitosn + " ");
+                        suma_num_int += digitosn;
+                    }
+                }
+                System.out.println("numeros pares: " + sumapar);
+                System.out.println("suma numeros pares: " + suma_num_int);
+            break;
+
+            case 33: // EJERCICIO 33
+                System.out.println(ut.GREEN_BOLD + "EJERCICIO 33" + ut.RESET);
+                System.out.println(
+                        """
+                                Realiza un programa que pinte la letra U por pantalla hecha con asteriscos. El Programa pedirá la altura. Fíjate que el
+                                programa inserta un espacio delante de la figura. Además se pintan dos asteriscos menos en la base para simular la curvatura
+                                de las esquinas inferiores
+                                    """);
+
+                numero = Integer.parseInt(System.console().readLine("Introduzca la altura de la U: "));
+                anchon = (int) Math.ceil(numero / 2);
+
+                for (i = 1; i <= numero; i++) {
+                    if (i == numero) {
+                        for (int j = 1; j <= numero; j++) {
+                            if (j == 1 || j == numero) {
+                                System.out.print(" ");
+                            } else
+                                System.out.print("* ");
+                        }
+                    } else {
+                        int n = ((numero % 2) == 0 ? (numero - 1) : numero);
+                        System.out.printf("*" + "%" + n + "s" + "*%n", ""); // % + ANCHO + s no me funciona, no se
+                                                                            // porque
+                    }
+                }
+
+            case 34: // EJERCICIO 34
+                System.out.println(ut.GREEN_BOLD + "EJERCICIO 33" + ut.RESET);
+                System.out.println(
+                        """
+                                Escribe un programa que pida dos números por teclado y que luego mezcle en dos números
+                                diferentes los dígitos pares y los impares. Se van comprobando los dígitos de la siguiente manera:
+                                primer dígito del primer número,primer digito del segundo número,segundo dígito del primer número, segundo
+                                dígito del segundo número... Para facilitar el ejercicio, podemos suponner que el usuario introducirá dos números de la misma
+                                longitud y que siempre habrá al menos un dígito par y uno impar. Usa long en lugar de int donde sea necesario
+                                para admitir números largos.
+                                    """);
+                sumapar = "";
+                String sumaimp = "";
+                String numerodosString = "";
+                do {
+                    numeroString = System.console().readLine("Introduce el primer número: ");
+                    numerodosString = System.console().readLine("Introduce el primer número: ");
+                    if (!(numeroString.length() == numerodosString.length())) {
+                        System.out.println("Introduce dos números de la misma longitud!!!");
+                    }
+                } while (!(numeroString.length() == numerodosString.length()));
+
+                numero = Integer.parseInt(numeroString);
+                numeron = Integer.parseInt(numerodosString);
+
+                for (i = 0; i < numeroString.length(); i++) {
+
+                    for (int j = 0; j < 2; j++) {
+
+                        if (j == 0) {
+                            digitosn = Character.getNumericValue(numeroString.charAt(i));
+                            if (digitosn % 2 == 0) {
+                                sumapar = (sumapar + "" + digitosn);
+                            } else {
+                                sumaimp = (sumaimp + "" + digitosn);
+                            }
+                        } else {
+                            digitosn = Character.getNumericValue(numerodosString.charAt(i));
+                            if (digitosn % 2 == 0) {
+                                sumapar = (sumapar + "" + digitosn);
+                            } else {
+                                sumaimp = (sumaimp + "" + digitosn);
+                            }
+                        }
+
+                    }
+
+                }
+                System.out.println(sumapar);
+                System.out.println(sumaimp);
 
         }
 
     }
-
+}
