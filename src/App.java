7eb366bc04ebed32de86fbda89b8e47c61d5441e
Paagola <victorpagola.w@gmail.com>
2025-11-09 20:39:27 +0100

    exercise 43 done

--- src/App.java
+++ src/App.java
@@ -1167,6 +1167,48 @@ public class App {
                         System.out.printf("%d es primo%n", i);
                     }
                 }
+                break;
+
+                case 43: // EJERCICIO 43
+                System.out.println(ut.GREEN_BOLD + "EJERCICIO 43" + ut.RESET);
+                System.out.println(
+                        """
+                                Escribe un programa que permita partir un número introducido por teclado en dos partes.
+                                Las posiciones se cuentan de izquierda a derecha empezando por el 1. Suponemos que el usuario
+                                introduce correctamente los datos, es decir, el número introducido tiene dos dígitos como
+                                mínimo y la posición en la que se parte el número está entre 2 y la longitud del número.
+                                No se permiten el uso de funciones de manejo de Strings.
+                                        """);
+                numero = Integer.parseInt(System.console().readLine("Por favor, introduzca un número entero positivo: "));
+                digitosn = Integer.parseInt(System.console().readLine("Introduzca la posicióna partir de la cual quiere partir el número: "));
+
+                aux = numero * 10 + 1;
+                aux_longReverse = 0;
+                int contador = 0;
+
+                while (aux > 0) {
+                    aux_longReverse = aux_longReverse * 10 + (aux % 10);
+                    aux /= 10;
+                    contador++;
+                }
+
+                numeron = 0;
+                int numeron2 = 0;
+
+                for (i = 1; i < contador; i++) {
+                    if (i < digitosn) {
+                        numeron = numeron * 10 + (int)(aux_longReverse % 10); 
+                    } else {
+                        numeron2 = numeron2 * 10 + (int)(aux_longReverse % 10); 
+                    }
+                    aux_longReverse /= 10;
+                }
+
+                System.out.printf("Los números partidos son el %d y el %d.", numeron, numeron2);
+
+
+
+                
         }
     }
 }
